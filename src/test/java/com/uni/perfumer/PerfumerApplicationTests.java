259464b593fb1605bf4b8ef6fcc262f655b1b958
root <jgh@gmail.com>
2023-01-21 15:19:16 +0900

    깃허브 연결

---
+++ src/test/java/com/uni/perfumer/PerfumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.perfumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
