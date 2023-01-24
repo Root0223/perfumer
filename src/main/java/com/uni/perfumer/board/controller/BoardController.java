1865795414af0307f900d6253bb0b0e8d91a8fce
root <jgh@gmail.com>
2023-01-24 22:09:23 +0900

    게시판 삭제 조회

--- src/main/java/com/uni/perfumer/board/controller/BoardController.java
+++ src/main/java/com/uni/perfumer/board/controller/BoardController.java
@@ -5,11 +5,12 @@ import com.uni.perfumer.board.model.dao.BoardMapper;
 import com.uni.perfumer.board.model.dto.ApiResponse;
 import com.uni.perfumer.board.model.dto.BoardDTO;
 import com.uni.perfumer.board.model.service.BoardService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 //@RequiredArgsConstructor
@@ -38,4 +39,32 @@ public class BoardController {
      }
      return responseEntity;
   }
-}
+
+  @PostMapping("/board")
+  public ResponseEntity<List<BoardDTO>> boardPost(@RequestBody BoardDTO boardDTO){
+      return new ResponseEntity<>(boardService.boardList(boardDTO), HttpStatus.OK);
+  }
+
+  @GetMapping("/board")
+    public ResponseEntity<List<BoardDTO>> boardGet(){
+      return new ResponseEntity<List<BoardDTO>>(boardService.boardList(new BoardDTO()), HttpStatus.OK);
+  }
+
+  @DeleteMapping("/board/{boardNo}")
+    public ResponseEntity<?> boardDelete(@PathVariable Integer boardNo){
+      ResponseEntity<ApiResponse> responseEntity;
+
+      try {
+          boardService.deleteBoard(boardNo);
+          responseEntity = new ResponseEntity<>(new ApiResponse(true,
+                  boardNo + "번째 글이 삭제 되었습니다"), HttpStatus.OK);
+       } catch (Exception e){
+          responseEntity = new ResponseEntity<>(new ApiResponse(false, e.getMessage()),HttpStatus.BAD_REQUEST);
+
+      }
+      return responseEntity;
+      }
+  }
+
+
+
